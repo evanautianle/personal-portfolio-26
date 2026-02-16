6447d680d931d0e742fad11b644e335ebbfe1791
Evan Au <93043846+evanautianle@users.noreply.github.com>
2026-02-17 01:09:02 +1300

    feat: add crewSpawnPoints config for captain, helm, and ops positions/rotations

---
+++ src/config/crewSpawnPoints.js
@@ -0,0 +1,14 @@
+export const crewSpawnPoints = {
+  captain: {
+    position: [0, 0, 8],
+    rotation: [0, Math.PI, 0],
+  },
+  helm: {
+    position: [-6, 0, -4],
+    rotation: [0, 0, 0],
+  },
+  ops: {
+    position: [6, 0, -4],
+    rotation: [0, 0, 0],
+  },
+};
