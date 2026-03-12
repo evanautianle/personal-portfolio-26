1bfe610d404175bbea4ebd421a701536b7eee8cf
Evan Au <93043846+evanautianle@users.noreply.github.com>
2026-03-12 19:41:30 +1300

    fix: works locally as well as deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Use different base for local and production
+const isProd = process.env.NODE_ENV === 'production';
+const base = isProd ? '/personal-portfolio-26/' : '/';
 
 export default defineConfig({
-  base: '/personal-portfolio-26/',
   plugins: [react()],
-})
+  base,
+});
